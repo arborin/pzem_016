24f32a53cbd77e828b95f8b01328c1fdf4dcd6b7
arborin <nika.kobaidze@gmail.com>
2021-09-14 23:48:47 +0400

    Clean code

--- serial_reader.py
+++ serial_reader.py
@@ -52,9 +52,11 @@ class Pzem:
             self.ser.write(self.read_command)
             self.device_response = self.ser.readline().hex()
             
-            self.responcse_parser()
-            self.print_vals()
-            self.send_data()
+            # Parse device responce and send it 
+            parse_result = self.responcse_parser()
+            if parse_result:
+                self.print_vals()
+                self.send_data()
 
 
     def responcse_parser(self):
@@ -83,6 +85,7 @@ class Pzem:
             pf = int(pf, 16)
             self.device_vals['pf'] = float(pf) / 100
 
+            return True
         else:
             print("ERROR IN RESPONSE")
             return False
@@ -105,61 +108,3 @@ url = 'example_url.com'
 
 dev = Pzem(port, url)
 dev.read_vals()
-
-
-
-
-
-
-
-
-# ser = serial.Serial('COM5', 9600, timeout=0.5)
-# print(ser)
-# time.sleep(0.1)
-
-# run = True
-
-# while run:
-#     # READ VALUES FROM DEVICE
-#     command = "\xf8\x04\x00\x00\x00\x0a\x64\x64"
-#     ser.write(serial.to_bytes([0xF8,0x04,0x00,0x00,0x00,0x0A,0x64,0x64]))
-
-#     # GET RESPONSE
-#     response = ser.readline().hex()
-#     print(type(response))
-#     print(response)
-    
-#     # SPLIT STING INTO LIST
-#     response_vals = re.findall('..', response)
-#     print(response_vals)
-    
-
-#     if len(l)>20:
-#         voltage = l[3] + l[4]
-#         voltage = int(voltage, 16)
-#         print("Voltage:\t{} V".format(float(voltage)/10))
-
-#         current = l[5] + l[6]
-#         current = int(current, 16)
-#         print("Current:\t{} A".format(float(current)/1000))
-
-#         power = l[7] + l[8] + l[9] + l[10]
-#         power = int(power, 16)
-#         print("Power:\t{} W".format(float(power)/10))
-
-#         energy = l[11] + l[12] + l[13] + l[14]
-#         energy = int(energy, 16)
-#         print("Energy:\t{} Wh".format(float(energy)))
-
-#         freq = l[15] + l[16] + l[17] + l[18]
-#         freq = int(freq, 16)
-#         print("Frequency:\t{} Hz".format(freq/10))
-
-#         pf = l[19] + l[20]
-#         pf = int(pf, 16)
-#         print("Power Factor:\t{}".format(float(pf)/100))
-
-#         print("\n\n\n")
-#         time.sleep(2)
-
-# ser.close()
